7af8e3ffcca9703764c30c69458e8bcfef279425
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-24 17:14:13 -0400

    Brought the html elements into javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const gameContainer = document.querySelector(".game-container");
+const startButton = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
+const playerOneInput = document.getElementById("player-one-input");
+const playerTwoInput = document.getElementById("player-two-input");
+const gameDisplay = document.getElementById("game-display");
