bd3c8d30831f957539ca62292b5e1c574807e1fe
Rob White <155823032+RobJoeeLee@users.noreply.github.com>
2025-03-25 10:03:42 -0400

    created the GameBoard function and used IIFE

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const resetButton = document.getElementById("reset-button");
 const playerOneInput = document.getElementById("player-one-input");
 const playerTwoInput = document.getElementById("player-two-input");
 const gameDisplay = document.getElementById("game-display");
+
+const GameBoard = (function(){
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const getBoard = function(){
+        return board;
+    }
+
+    const updateBoard = function(index, marker){
+        if(board[index] === ""){
+            board[index] = marker;
+            return true;
+        }
+        return false;
+    };
+
+    const resetBoard = function(){
+        board = ["", "", "", "", "", "", "", "", ""];
+    };
+
+    return { getBoard, updateBoard, resetBoard };
+})();
